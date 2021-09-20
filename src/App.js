0167efe82b8b3445b99d970b84f6f63001fffa75
djevy <60827567+djevy@users.noreply.github.com>
2021-09-20 11:49:02 +0100

    Worked on navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+
 import Navbar from './Components/Navbar/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <Navbar/>
-      
+      <Switch>
+        <Route path="/">
+
+        </Route>
+        {/* <Route path="*" component={NotFound} /> */}
+      </Switch>
+    </Router>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import "./Navbar"
+import "./Navbar.scss"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return ( 
         <section id="navbar">
-            
+            <div id="logo">
+                <h1>DE</h1>
+            </div>
+            <div id="links">
+                <Link to="/" className="Link">Home</Link>
+                <Link to="/about" className="Link">About</Link>
+                <Link to="/projects" className="Link">Projects</Link>
+                <Link to="/articles" className="Link">Resume</Link>
+                <p>Contact Me</p>
+            </div>
+
         </section>
      );
 }
